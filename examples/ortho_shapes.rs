5c74e39af96f3d899fcb495afc4560efead523e2
Jonathan Johnson <jon@nilobject.com>
2020-03-23 16:02:03 -0700

    Extraneous include.

--- examples/ortho_shapes.rs
+++ examples/ortho_shapes.rs
@@ -1,5 +1,4 @@
 use kludgine::prelude::*;
-use kludgine::async_trait::async_trait;
 
 fn main() {
     Runtime::new(SingleWindowApplication::<OrthoShapes>::default()).run();
@@ -22,20 +21,20 @@ impl Window for OrthoShapes {
         if !self.created_shapes {
             self.created_shapes = true;
             let mut last_mesh_id = None;
-            for i in 1..10 {
+            for _ in 1..10 {
                 let material = Material::Solid {
                     color: Color::new_rgba(255, 0, 0, 255),
                 };
                 let mesh = scene.screen().create_mesh(
                     Shape::rect(&Rect::new(
-                        Point2d::new(-16.0, -16.0),
+                        Point2d::new(0.0, 0.0),
                         Size2d::new(32.0, 32.0),
                     )),
                     material,
                 );
                 scene
                     .screen()
-                    .place_mesh(&mesh, last_mesh_id, Point2d::new(48.0, 0.0), Deg(5.0).into(), 1.1);
+                    .place_mesh(&mesh, last_mesh_id, Point2d::new(32.0, 0.0), Deg(5.0).into(), 1.2);
                     last_mesh_id = Some(mesh.id);
             }
         }
