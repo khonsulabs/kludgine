ea564bee7f3cc63946c9b84f16c1a1ded8035651
Jonathan Johnson <jon@khonsulabs.com>
2021-06-07 20:06:21 -0700

    Introduced xtask crate
    * Moved kludgine into a subfolder so that this can transition to a workspace. No code changes except changing the font references.
    * Switched code coverage to the shared logic.
    * Migrated from `cargo make` to build the shaders to xtask, although long term I want to remove the need for glslc.

--- kludgine/src/text/bundled_fonts.rs
+++ kludgine/src/text/bundled_fonts.rs
@@ -21,38 +21,38 @@ use crate::{include_font, text::font::Font};
 lazy_static! {
     /// Roboto Regular font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO: Font = include_font!("../../fonts/roboto/Roboto-Regular.ttf");
+    pub static ref ROBOTO: Font = include_font!("../../../fonts/roboto/Roboto-Regular.ttf");
     /// Roboto Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-Italic.ttf");
+    pub static ref ROBOTO_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-Italic.ttf");
     /// Roboto Black font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_BLACK: Font = include_font!("../../fonts/roboto/Roboto-Black.ttf");
+    pub static ref ROBOTO_BLACK: Font = include_font!("../../../fonts/roboto/Roboto-Black.ttf");
     /// Roboto Black-Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_BLACK_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-BlackItalic.ttf");
+    pub static ref ROBOTO_BLACK_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-BlackItalic.ttf");
     /// Roboto Bold font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_BOLD: Font = include_font!("../../fonts/roboto/Roboto-Bold.ttf");
+    pub static ref ROBOTO_BOLD: Font = include_font!("../../../fonts/roboto/Roboto-Bold.ttf");
     /// Roboto Bold-Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_BOLD_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-BoldItalic.ttf");
+    pub static ref ROBOTO_BOLD_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-BoldItalic.ttf");
     /// Roboto Light font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_LIGHT: Font = include_font!("../../fonts/roboto/Roboto-Light.ttf");
+    pub static ref ROBOTO_LIGHT: Font = include_font!("../../../fonts/roboto/Roboto-Light.ttf");
     /// Roboto Light-Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_LIGHT_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-LightItalic.ttf");
+    pub static ref ROBOTO_LIGHT_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-LightItalic.ttf");
     /// Roboto Medium font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_MEDIUM: Font = include_font!("../../fonts/roboto/Roboto-Medium.ttf");
+    pub static ref ROBOTO_MEDIUM: Font = include_font!("../../../fonts/roboto/Roboto-Medium.ttf");
     /// Roboto Medium-Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_MEDIUM_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-MediumItalic.ttf");
+    pub static ref ROBOTO_MEDIUM_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-MediumItalic.ttf");
     /// Roboto  font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_THIN: Font = include_font!("../../fonts/roboto/Roboto-Thin.ttf");
+    pub static ref ROBOTO_THIN: Font = include_font!("../../../fonts/roboto/Roboto-Thin.ttf");
     /// Roboto Thin-Italic font, licensed under APL 2.0, feature flag `bundled-fonts-roboto`
     #[cfg(feature="bundled-fonts-roboto")]
-    pub static ref ROBOTO_THIN_ITALIC: Font = include_font!("../../fonts/roboto/Roboto-ThinItalic.ttf");
+    pub static ref ROBOTO_THIN_ITALIC: Font = include_font!("../../../fonts/roboto/Roboto-ThinItalic.ttf");
 }
---
+++ xtask/src/main.rs
@@ -0,0 +1,52 @@
+use devx_cmd::run;
+use khonsu_tools::{anyhow, code_coverage::CodeCoverage};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Args {
+    CompileShaders,
+    GenerateCodeCoverageReport,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::from_args();
+    match args {
+        Args::CompileShaders => compile_shaders()?,
+        Args::GenerateCodeCoverageReport => CodeCoverage::<CodeCoverageConfig>::execute()?,
+    };
+    Ok(())
+}
+
+fn compile_shaders() -> Result<(), devx_cmd::Error> {
+    println!("Building sprite fragment shader");
+    run!(
+        "glslc",
+        "kludgine/src/sprite/shaders/sprite.frag",
+        "-o",
+        "kludgine/src/sprite/shaders/sprite.frag.spv",
+    )?;
+    println!("Building sprite vertex shader");
+    run!(
+        "glslc",
+        "kludgine/src/sprite/shaders/sprite.vert",
+        "-o",
+        "kludgine/src/sprite/shaders/sprite.vert.spv",
+    )?;
+    println!("Building sprite SRGB vertex shader");
+    run!(
+        "glslc",
+        "kludgine/src/sprite/shaders/sprite-srgb.vert",
+        "-o",
+        "kludgine/src/sprite/shaders/sprite-srgb.vert.spv",
+    )?;
+
+    Ok(())
+}
+
+struct CodeCoverageConfig;
+
+impl khonsu_tools::code_coverage::Config for CodeCoverageConfig {
+    fn ignore_paths() -> Vec<String> {
+        vec![String::from("kludgine/examples/*")]
+    }
+}
