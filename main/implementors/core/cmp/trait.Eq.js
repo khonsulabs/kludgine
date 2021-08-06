bed4bfa0a1c19cf56be840ea5b518e24ede6a4c0
kl-botsu <botsu@khonsulabs.com>
2021-08-06 14:45:30 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@55e1237d8cc8a948489cda0e98d7bd62588d90e3 🚀

--- main/implementors/core/cmp/trait.Eq.js
+++ main/implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kludgine_app"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"kludgine_app/event/enum.EventStatus.html\" title=\"enum kludgine_app::event::EventStatus\">EventStatus</a>","synthetic":false,"types":["kludgine_app::window::event::EventStatus"]}];
+implementors["kludgine_app"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"kludgine_app/event/enum.EventStatus.html\" title=\"enum kludgine_app::event::EventStatus\">EventStatus</a>","synthetic":false,"types":["kludgine_app::window::event::EventStatus"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
