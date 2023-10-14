9174bd0dd960a9e74df7d813ae361e2758655f40
kl-botsu <botsu@khonsulabs.com>
2023-10-14 22:07:55 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@e1a29844d7bd0cfca66b775ce22d59b6ac8108c4 🚀

--- main/src-files.js
+++ main/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "kludgine":["",[],["app.rs","atlas.rs","buffer.rs","lib.rs","pipeline.rs","pod.rs","render.rs","sealed.rs","shapes.rs","text.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
