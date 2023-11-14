3c04c24cdfbc1e79479fe693ee9826c4dbb8a198
kl-botsu <botsu@khonsulabs.com>
2023-11-14 07:05:11 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@09790aafb5a9c3b0da034387adead9960eb06bc7 🚀

--- main/kludgine/tilemap/sidebar-items.js
+++ main/kludgine/tilemap/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["TILE_SIZE"],"enum":["TileKind","TileMapFocus"],"fn":["draw"],"struct":["ObjectId","ObjectInfo","ObjectLayer","TileOffset","Tiles"],"trait":["Layer","Layers","Object"]};
+window.SIDEBAR_ITEMS = {"constant":["TILE_SIZE"],"enum":["TileKind","TileMapFocus"],"fn":["draw","translate_coordinates"],"struct":["ObjectId","ObjectInfo","ObjectLayer","TileOffset","Tiles"],"trait":["Layer","Layers","Object"]};
