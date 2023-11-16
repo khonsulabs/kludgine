05dcc1e4751e8cf86ea59bd4271f3e6909b341e8
kl-botsu <botsu@khonsulabs.com>
2023-11-16 23:34:22 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@220df4ed07fcf86459a34591a7923d7ddb25e67e 🚀

--- main/implementors/core/cmp/trait.Ord.js
+++ main/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"kludgine":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"kludgine/tilemap/struct.ObjectId.html\" title=\"struct kludgine::tilemap::ObjectId\">ObjectId</a>"]]
+"kludgine":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"kludgine/tilemap/struct.ObjectId.html\" title=\"struct kludgine::tilemap::ObjectId\">ObjectId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
