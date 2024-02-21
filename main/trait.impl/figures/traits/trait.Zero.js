b8d6fe12aa800bc46753e945a739ed534a1dee94
kl-botsu <botsu@khonsulabs.com>
2024-02-21 20:06:34 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@ba1ab2b57c16b8be6a891002d84607ef76cef064 🚀

--- main/trait.impl/figures/traits/trait.Zero.js
+++ main/trait.impl/figures/traits/trait.Zero.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"kludgine":[["impl&lt;Unit&gt; Zero for <a class=\"struct\" href=\"kludgine/shapes/struct.CornerRadii.html\" title=\"struct kludgine::shapes::CornerRadii\">CornerRadii</a>&lt;Unit&gt;<span class=\"where fmt-newline\">where\n    Unit: Zero,</span>"]]
+"kludgine":[["impl&lt;Unit&gt; Zero for <a class=\"struct\" href=\"kludgine/shapes/struct.CornerRadii.html\" title=\"struct kludgine::shapes::CornerRadii\">CornerRadii</a>&lt;Unit&gt;<div class=\"where\">where\n    Unit: Zero,</div>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
