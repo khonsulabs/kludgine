539c6c6fb3688656442ba4dcea6685771341c4d1
kl-botsu <botsu@khonsulabs.com>
2024-04-11 16:29:52 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@f63027b3b4cabad073663b7124765454b0be3a89 🚀

--- main/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ main/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"kludgine":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"kludgine/sprite/struct.SpriteMap.html\" title=\"struct kludgine::sprite::SpriteMap\">SpriteMap</a>&lt;T&gt;"]]
+"kludgine":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"kludgine/sprite/struct.SpriteMap.html\" title=\"struct kludgine::sprite::SpriteMap\">SpriteMap</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
