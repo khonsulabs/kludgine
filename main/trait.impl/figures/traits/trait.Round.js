b1765f704f1d3b1982e750839d6475e017834d95
kl-botsu <botsu@khonsulabs.com>
2024-07-26 15:46:31 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@3a33c18c2875146371c5f483dcb80e2b4ca5a7da 🚀

---
+++ main/trait.impl/figures/traits/trait.Round.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kludgine":[["impl&lt;Unit&gt; Round for <a class=\"struct\" href=\"kludgine/shapes/struct.CornerRadii.html\" title=\"struct kludgine::shapes::CornerRadii\">CornerRadii</a>&lt;Unit&gt;<div class=\"where\">where\n    Unit: Round,</div>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
