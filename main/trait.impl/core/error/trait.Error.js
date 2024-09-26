2dbf587e92e9542fa69ef27f7706e2409e101169
kl-botsu <botsu@khonsulabs.com>
2024-09-26 17:04:12 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@6deb4b041ae48a6a0ad4ca495dcce7dcce7d5340 🚀

---
+++ main/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kludgine":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"kludgine/app/enum.UnrecoverableError.html\" title=\"enum kludgine::app::UnrecoverableError\">UnrecoverableError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ main/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kludgine":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"kludgine/app/enum.UnrecoverableError.html\" title=\"enum kludgine::app::UnrecoverableError\">UnrecoverableError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
