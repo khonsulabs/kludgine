958e9ebb8437b8bb23cc6fc2bb098a6348cff280
kl-botsu <botsu@khonsulabs.com>
2024-11-29 16:54:33 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@586c0df7ab0999c613424fc0d292502960676ade 🚀

--- main/trait.impl/core/fmt/trait.Display.js
+++ main/trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["kludgine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"kludgine/app/enum.UnrecoverableError.html\" title=\"enum kludgine::app::UnrecoverableError\">UnrecoverableError</a>"]]]]);
+    var implementors = Object.fromEntries([["kludgine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"kludgine/app/enum.UnrecoverableError.html\" title=\"enum kludgine::app::UnrecoverableError\">UnrecoverableError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ main/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["kludgine",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"kludgine/sprite/struct.SpriteMap.html\" title=\"struct kludgine::sprite::SpriteMap\">SpriteMap</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["kludgine",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"kludgine/sprite/struct.SpriteMap.html\" title=\"struct kludgine::sprite::SpriteMap\">SpriteMap</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
