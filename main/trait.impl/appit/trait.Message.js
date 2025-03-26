b6ee21697e833c0a9a840b811a2bab9be7e495f3
kl-botsu <botsu@khonsulabs.com>
2025-03-26 21:01:56 +0000

    Deploying to gh-pages from @ khonsulabs/kludgine@5507755a6e8aad28303f5828af019516457194f9 🚀

--- main/trait.impl/appit/trait.Message.js
+++ main/trait.impl/appit/trait.Message.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["kludgine",[["impl&lt;User&gt; <a class=\"trait\" href=\"kludgine/app/trait.Message.html\" title=\"trait kludgine::app::Message\">Message</a> for <a class=\"struct\" href=\"kludgine/app/struct.AppEvent.html\" title=\"struct kludgine::app::AppEvent\">AppEvent</a>&lt;User&gt;<div class=\"where\">where\n    User: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + 'static,</div>"]]]]);
+    var implementors = Object.fromEntries([["kludgine",[["impl&lt;User&gt; <a class=\"trait\" href=\"kludgine/app/trait.Message.html\" title=\"trait kludgine::app::Message\">Message</a> for <a class=\"struct\" href=\"kludgine/app/struct.AppEvent.html\" title=\"struct kludgine::app::AppEvent\">AppEvent</a>&lt;User&gt;<div class=\"where\">where\n    User: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + 'static,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/core/cmp/trait.Ord.js
+++ main/trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["kludgine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"kludgine/tilemap/struct.ObjectId.html\" title=\"struct kludgine::tilemap::ObjectId\">ObjectId</a>"]]]]);
+    var implementors = Object.fromEntries([["kludgine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"kludgine/tilemap/struct.ObjectId.html\" title=\"struct kludgine::tilemap::ObjectId\">ObjectId</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
